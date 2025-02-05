3bee86295bf872a47e4d9187911772286d16e338
dario.junior <dariojr1233@gmail.com>
2025-02-05 19:40:44 -0300

    Railway deploy

--- src/main/java/com/br/thomasvcgApi/ThomasvcgApiApplication.java
+++ src/main/java/com/br/thomasvcgApi/ThomasvcgApiApplication.java
@@ -1,5 +1,6 @@
 package com.br.thomasvcgApi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class 	ThomasvcgApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		String dbUrl = dotenv.get("DATABASE_URL");
+		String dbUser = dotenv.get("DATABASE_USERNAME");
+		String dbPassword = dotenv.get("DATABASE_PASSWORD");
+
+		System.out.println("Database URL: " + dbUrl);
+		System.out.println("Database User: " + dbUser);
+		System.out.println("Database Password: " + dbPassword);
 		SpringApplication.run(ThomasvcgApiApplication.class, args);
 	}
 
